a0d2e690ac7ef44da4849c5637c0fc0b89a7fe6c
alwaysmakemistakes <114398958+alwaysmakemistakes@users.noreply.github.com>
2023-06-19 13:34:03 +0300

    Add files via upload

---
+++ app/config.py
@@ -0,0 +1,8 @@
+SECRET_KEY = '9695cce7d2daa4d9c7f952d1cca92b6c9183e5907262017d987e60de07801749'
+
+MYSQL_USER = 'std_2184_labs'
+MYSQL_PASSWORD = '22222222'
+MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
+MYSQL_DATABASE = 'std_2184_labs'
+
+ADMIN_ROLE_ID = 1
